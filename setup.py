0d2c41a7edaf25b35a63e8743690531f2af2fa63
Balázs Indig <indba@digitus.itk.ppke.hu>
2020-11-27 00:05:01 +0100

    Redesign Makefile

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8, vim: expandtab:ts=4 -*-
 
+import sys
 import setuptools
-from emconspy import __version__
+import importlib.util
+
+
+def import_pyhton_file(module_name, file_path):
+    # Import module from file: https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+    return module
+
 
 with open('README.md') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='emconspy',
-    version=__version__,
+    # Get version without actually importing the module
+    version=getattr(import_pyhton_file('version', 'emconspy/version.py'), '__version__'),
     author='dlazesz',  # Will warn about missing e-mail
     description='A wrapper implemented in Python for emCons (Berkeley parser a.k.a. Product Parser)',
     long_description=long_description,
@@ -24,8 +36,8 @@ setuptools.setup(
         'Operating System :: POSIX :: Linux',
     ],
     python_requires='>=3.6',
-    install_requires=['pyjnius>=1.3.0',  # TODO: List dependencies at only one file requirements.txt vs. setup.py
-                      'xtsv>=1.0,<2.0',
+    install_requires=['xtsv>=1.0.0,<2.0.0',
+                      'pyjnius>=1.3.0',
                       ],
     include_package_data=True,
     entry_points={
